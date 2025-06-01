55c16e9cc97002e83d3b4c8982059fa4f5952de7
Yuvraj Pratap Singh <147310860+Yuvrajpr4tap@users.noreply.github.com>
2025-06-01 18:54:12 +0530

    Update euler4.py
    4	Largest Palindrome Product

--- euler4.py
+++ euler4.py
@@ -1,6 +1,6 @@
 largest = 0
 for i in range(999, 99, -1):
-    for j in range(i, 99, -1):  # avoid redundant pairs
+    for j in range(i, 99, -1):  # avoid redundant pairs.
         product = i * j
         if product <= largest:
             break  # since j is decreasing, products will decrease
